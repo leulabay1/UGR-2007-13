f93fad405aaca09f03247c0228d05ca2a0e88c49
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2023-01-27 23:08:27 +0300

    Add files via upload

---
+++ Src/discription.js
@@ -0,0 +1,184 @@
+const nextBtn = document.querySelector("#forwardIcon0");
+const prevbtn = document.querySelector("#backIcon0");
+const carouselSlide = document.querySelector("#slider");
+const carouselImage = document.querySelector("#lastClone");
+
+
+let carouselImages = ["image/Asus.jpg", "image/acernitro.jpg", "image/alienware.jpg"];
+
+var counter = 0;
+
+//Button Listeners
+nextBtn.addEventListener('click', () => {
+    carouselImage.setAttribute("src", carouselImages[counter]);
+    counter++;
+    if (counter > carouselImages.length-1){
+        counter = 0;
+    }
+});
+prevbtn.addEventListener('click', () => {
+   carouselImage.setAttribute("src", carouselImages[counter]);
+   if (counter = -1 ){
+       counter = carouselImages.length-1;
+   }
+});
+
+var Laptops = [
+    {
+        name: "Lenevo | Thinkpad ",
+        Processor: "core i5 10th",
+        Storage: "512 SSD", 
+        RAM: "8GB",
+        Battery:"8 hours and above",
+     },
+     {
+        name: "Asus | Tuf",
+        Processor: "core i7 11th",
+        Storage: "1 Tera SSD", 
+        RAM: "16GB",
+        Battery:"6 hours and above"
+     },
+     {
+        name: "Lenevo | IdeaPad",
+        Processor: "core i3 8th",
+        Storage: "256 SSD", 
+        RAM: "8GB",
+        Battery:"8 hours and above"
+     },
+     {
+        name: "Acer | Magnit",
+        Processor: "core i3 11th",
+        Storage: "512 SSD", 
+        RAM: "12GB",
+        Battery:"8 hours and above"
+     },
+     {
+        name: "Lenevo | IdeaPad",
+        Processor: "core i5 10th ",
+        Storage: "512 SSD", 
+        RAM: "8GB",
+        Battery:"4 hours and above"
+     },
+     {
+        name: "Asus | republic",
+        Processor: "Celeron | Used",
+        Storage: "512 SSD", 
+        RAM: "8GB",
+        Battery:"8 hours and above"
+     },
+     {
+        name: "MSI | Leopard",
+        Processor: "core i5",
+        Storage: "512 SSD", 
+        RAM: "16GB",
+        Battery:"10 hours and above"
+     },
+     {
+        name: "Asus | Vivobook",
+        Processor: "core i7",
+        Storage: "512 SSD", 
+        RAM: "4GB",
+        Battery:"8 hours and above"
+     },
+     {
+        name: "HP | Alien Ware",
+        Processor: "core i7 11th",
+        Storage: "512 SSD", 
+        RAM: "16GB",
+        Battery:"4 hours and above"
+     },
+     {
+        name: "Asus | republic",
+        Processor: "core i7",
+        Storage: "512 SSD", 
+        RAM: "8GB",
+        Battery:"5 hours and above"
+     },
+     {
+        name: "MSI | Leopard",
+        Processor: "core i7",
+        Storage: "512 SSD", 
+        RAM: "12GB",
+        Battery:"8 hours and above"
+     },
+     {
+        name: "Acer | Nitro ",
+        Processor: "core i7",
+        Storage: "256 SSD", 
+        RAM: "8GB",
+        Battery:"6 hours and above"
+     },
+     {
+        name: "Lenevo | IdeaPad",
+        Processor: "core i5",
+        Storage: "512 HDD", 
+        RAM: "8GB",
+        Battery:"8 hours and above"
+     },
+     {
+        name: "Asus | republic",
+        Processor: "core i5",
+        Storage: "512 SSD", 
+        RAM: "8GB",
+        Battery:"10 hours and above"
+     },
+     {
+        name: "HP | Omen",
+        Processor: "core i5",
+        Storage: "128 SSD", 
+        RAM: "8GB",
+        Battery:"8 hours and above"
+     },
+     {
+        name: "HP | Envy",
+        Processor: "core i5",
+        Storage: "256 SSD", 
+        RAM: "4GB",
+        Battery:"12 hours and above"
+     },
+     {
+        name: "Asus | elite",
+        Processor: "core i3",
+        Storage: "512 HDD", 
+        RAM: "4GB",
+        Battery:"8 hours and above"
+     },
+     {
+        name: "HP | NoteBook",
+        Processor: "core i3",
+        Storage: "512 SSD", 
+        RAM: "16GB",
+        Battery:"8 hours and above"
+     },
+     {
+        name: "Lenovo | ideaPad",
+        Processor: "core i3",
+        Storage: "1024 SSD", 
+        RAM: "8GB",
+        Battery:"8 hours and above"
+     },
+     {
+        name: "Asus | Vivobook",
+        Processor: "core i3",
+        Storage: "512 SSD", 
+        RAM: "8GB",
+        Battery:"8 hours and above"
+     }
+]
+function get(){
+    var para = new URLSearchParams(window.location.search);
+    var pass = parseInt(para.get("id"));
+    var brand = document.getElementById('r1c2');
+    var Processor = document.getElementById('r2c2');
+    var Storage = document.getElementById('r3c2');
+    var RAM = document.getElementById('r4c2');
+    var Battery = document.getElementById('r5c2');
+    var laptop = Laptops[pass];
+    console.log(brand);
+
+    brand.innerHTML = laptop.name;
+    Processor.innerHTML=laptop.Processor;
+    Storage.innerHTML=laptop.Storage;
+    RAM.innerHTML=laptop.RAM;
+    Battery.innerHTML=laptop.Battery;
+}
